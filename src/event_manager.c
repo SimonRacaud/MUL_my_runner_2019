1d508b069a860597275bdca82273484a5ebe8b33
RACAUD Simon <simonracaud@gmail.com>
2019-12-12 12:49:29 +0100

    FEAT(event_manager : get data event keyboard and mouseclick)

--- src/event_manager.c
+++ src/event_manager.c
@@ -15,13 +15,25 @@ static void close(window_t *w)
 
 static void manage_mouse_click(window_t *w, sfEvent *event)
 {
+    sfMouseButtonEvent evt = event->mouseButton;
 
+    my_printf("Mouse click: x %d y %d \n", evt.x, evt.y);
 }
 
 static void manage_keyrelease(window_t *w, sfEvent *event)
 {
     if (event->key.code == sfKeyEscape)
         close(w);
+    else if (event->key.code == sfKeySpace)
+        my_putstr("KEY SPACE\n");
+    if (event->key.code == sfKeyLeft)
+        my_putstr("Key LEFT\n");
+    else if (event->key.code == sfKeyRight)
+        my_putstr("Key RIGHT\n");
+    if (event->key.code == sfKeyUp)
+        my_putstr("Key UP\n");
+    else if (event->key.code == sfKeyDown)
+        my_putstr("Key DOWN\n");
 }
 
 static void analyse_events(window_t *w, sfEvent *event)
