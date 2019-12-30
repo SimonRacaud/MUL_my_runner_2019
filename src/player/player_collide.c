6b71409477a51d725fc03fade04d59d150de92de
RACAUD Simon <simonracaud@gmail.com>
2019-12-30 13:34:19 +0100

    FEAT(new texture of blocks), FIX(coin remove (check two column)), FEAT(new map : stage01.map)

--- src/player/player_collide.c
+++ src/player/player_collide.c
@@ -8,22 +8,39 @@
 #include "my_runner.h"
 #include "player.h"
 
+static sfVector2i coin_find(map_t *map, player_t *player,
+window_t *w)
+{
+    sfVector2i block_coord = {0, 0};
+    sfVector2f coorda;
+    sfVector2f coordb;
+
+    coorda.y = player->obj->pos.y + player->obj->size.y - 5;
+    coorda.x = player->obj->pos.x + player->obj->size.x;
+    coordb.y = player->obj->pos.y + player->obj->size.y - 5;
+    coordb.x = player->obj->pos.x;
+    while (1) {
+        if (map_get_typeblock(map, w, &coorda) == BLOCK_COIN ||
+        map_get_typeblock(map, w, &coordb) == BLOCK_COIN) {
+            block_coord = map_get_block_coord(map, w, &coorda);
+            break;
+        }
+        coorda.y -= map->block_size;
+        coordb.y -= map->block_size;
+        if (coorda.y < player->obj->pos.y || coordb.y < player->obj->pos.y)
+            break;
+    }
+    return block_coord;
+}
+
 static void coin_add(map_t *map, player_t *player, window_t *w)
 {
-    sfVector2f coord;
-    sfVector2i block_coord;
+    sfVector2i block_coord = coin_find(map, player, w);
 
     w->game.coin_counter++;
     w->soundm.play(&w->soundm, SOUND_COIN);
-    coord.y = player->obj->pos.y + player->obj->size.y - 5;
-    coord.x = player->obj->pos.x + player->obj->size.x;
-    while (map_get_typeblock(map, w, &coord) != BLOCK_COIN) {
-        coord.y -= map->block_size;
-        if (coord.y < player->obj->pos.y)
-            break;
-    }
-    block_coord = map_get_block_coord(map, w, &coord);
-    map->map[block_coord.x][block_coord.y] = map->type_block[3];
+    if (!(block_coord.x == 0 && block_coord.y == 0))
+        map->map[block_coord.x][block_coord.y] = map->type_block[3];
 }
 
 int player_check_collision(player_t *player, map_t *map, window_t *w)
